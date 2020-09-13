2327191dfae759138b29218c28eb616769628978
rakeshkr114 <M1039158@mindtree.com>
2020-09-13 19:55:42 +0530

    Removed refreshScope as, Spring 2 & > uses management.endpoints.web.exposure.include to refresh configs

--- src/main/java/com/example/configclient/resources/MessageResource.java
+++ src/main/java/com/example/configclient/resources/MessageResource.java
@@ -1,12 +1,10 @@
 package com.example.configclient.resources;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
 @RestController
 @RequestMapping("/rest")
 public class MessageResource {
